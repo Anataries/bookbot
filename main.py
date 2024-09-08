d3a97abfd080ab261c729d980fc2ffdb2f564d13
Anataries <c61.smith@live.com.au>
2024-09-08 21:30:50 +1000

    Added word reader to main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    num_words = get_num_words(file_contents)
+    print(f"This book has {num_words} words")
+    
+def get_num_words(file_contents):
+    num_words = file_contents.split()
+    return len(num_words)
 
 #This line calls the main function
-main()
+if __name__ == "__main__":
+    main()
 
