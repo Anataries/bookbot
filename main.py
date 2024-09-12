6ceda400864cec42ed7bde884efee976843ee346
Anataries <c61.smith@live.com.au>
2024-09-12 21:07:26 +1000

    Char count in ascending order

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_count = count_characters(text) # Call your function here
-    print(f"{num_words} words found in this document")
-    print(char_count)
+    char_count = count_characters(text) 
+    print(f"{num_words} words found in the document")
+    #print("\n")
+    print_char_counts(char_count)
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -29,6 +32,14 @@ def count_characters(text):
             char_count[lowered_char] = 1
     return char_count
 
+def print_char_counts(char_count):
+    # Convert dictionary to list of tuples
+    char_list = list(char_count.items())
+    char_list.sort(key=lambda x: x[1], reverse=True)
+
+    for character, count in char_list:
+        if character.isalpha():
+            print(f"The '{character.lower()}' character was found {count} times")
 
 #This line calls the main function
 if __name__ == "__main__":
